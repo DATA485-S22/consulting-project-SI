c3261e7255545adec7397f21b1d8c02e54cd84bc
jshifman <josephshifman@gmail.com>
2022-03-06 14:55:14 -0800

    cleaning course detail

--- joseph_explore.R
+++ joseph_explore.R
@@ -2,6 +2,7 @@
 library(tidyverse)
 library(magrittr) # Allows %<>% notation to update lhs object with resulting value
 library(naniar)
+library(Hmisc) # %nin%
 
 program <- read.csv("data/Student Program.csv")
 
@@ -23,33 +24,31 @@ program %>%
   group_by(Major.1.STEM.Flag) %>%
   summarise(no_rows = length(Major.1.STEM.Flag))
 
-program$Major.1.STEM.Flag <- replace(program$Major.1.STEM.Flag, program$Major.1.STEM.Flag == "", "N")
+#########################################################
+courses <- read.csv("data/Course Detail.csv")
 
-ggplot(data = program) +
-  geom_bar(aes(x = Academic.Level.Code))
+length(unique(courses$Course.Subject.and.Number))
 
-program %>%
-  group_by(Random.Student.ID) %>%
-  summarise(no_rows = length(Random.Student.ID))
+courses <- courses %>%
+  mutate(course_num_clean = str_extract(Course.Catalog.Number, "\\d+"))
+courses$course_num_clean <- factor(courses$course_num_clean)
+levels(courses$course_num_clean)
 
-# Keep only most recent term record per student
-# Consider re-doing using `Term`
-program$College.Year <- factor(program$College.Year,
-                               levels = c("2011-12", "2012-13", "2013-14", "2014-15", "2015-16",
-                                          "2016-17", "2017-18", "2018-19", "2019-20", "2020-21",
-                                          "2021-22"),
-                               ordered = is.ordered(program$College.Year))
-program$Term.Type <- factor(program$Term.Type,
-                       levels = c("Intersession", "Summer", "Fall", "Winter", "Spring"),
-                       ordered = is.ordered(program$Term.Type))
-program <- program %>%
-  group_by(Random.Student.ID) %>%
-  arrange(College.Year, Term.Type) %>%
-  filter(row_number()==1)
+courses <- courses %>%
+  filter(course_num_clean %nin% c(189, 289, 389, 489, 589, 689, #internship
+                               198, 298, 398, 498, 598, 698, #special topics
+                               199, 299, 399, 499, 599, #special problems
+                               696, 697, 699)) #independent study
+courses$course_num_clean <- droplevels(courses$course_num_clean)
+levels(courses$course_num_clean)
 
-program %>%
-  group_by(Random.Student.ID) %>%
-  summarise(no_rows = length(Random.Student.ID))
+courses <- courses %>%
+  filter(Term.Year >= 2016)
 
-# Condiser using conditional logistic regression, matching students who did/did not
-# attend SI, matching on some vars
+courses$Term.Type <- factor(courses$Term.Type)
+levels(courses$Term.Type)
+courses <- courses %>%
+  filter(Term.Type %nin% c("Summer", "Winter"))
+courses$Term.type <- droplevels(courses$Term.Type)
+levels(courses$Term.Type)
+
