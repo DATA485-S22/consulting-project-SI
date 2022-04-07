4efc17ec468e89fe3597f978cddebd923f318614
jshifman <josephshifman@gmail.com>
2022-04-07 10:26:43 -0700

    Added to data management

--- data_management.R
+++ data_management.R
@@ -1,14 +1,18 @@
 # Data Management File
+library(tidyverse)
+library(Hmisc) # Enables %nin% notation
+library(readxl) # read Excel file
 
 ###########################################################################################
 # Course Level Data
 ###########################################################################################
-grades <- read.csv("data/grades.csv")
+grades <- read_xlsx("data/Student Grade.xlsx")
 
-course.level <- grades %>% group_by(Random.Course.ID, Term.Year, Term.Type) %>%
+course.level <- grades %>% group_by(`Random Course ID`, `Term Year`, `Term Type`) %>%
   summarise(class.size = n(),
-            class.average = sum(Student.Class.Grade.Point.per.Unit)/class.size,
-            dwf.rate = sum(Grade.DFW.Count)/class.size)
+            class.average = sum(`Student Class Grade Point per Unit`)/class.size,
+            dwf.rate = sum(`Grade DFW Count`)/class.size) %>%
+  rename(Random.Course.ID = `Random Course ID`)
 si_visit <- read.csv('data/SLC Appointment.csv')
 
 temp3 <- si_visit %>% group_by(Random.Course.ID) %>%
@@ -24,6 +28,66 @@ course.level <- left_join(course.level, course.detail, by = 'Random.Course.ID')
 course.level$n[is.na(course.level$n)] <- 0
 course.level$SI.Component.Flag <- course.level$n
 course.level$SI.Component.Flag[course.level$SI.Component.Flag > 0] <- 1
-course.level$SI.Component.Flag <- as.factor(course.level$SI.Component.Flag)
+course.level$SI.Component.Flag <- factor(course.level$SI.Component.Flag)
 
 write.csv(course.level, 'data/course_level.csv')
+
+################################################################################
+# Cleaned Student Profiles
+################################################################################
+
+# Student Program Dataset
+program <- read.csv("data/Student Program.csv")
+program.clean <- filter(program, Term.Year >= 2016) %>% # SI classes start in 2016
+  mutate(Term.Type = factor(Term.Type), .keep = "unused") %>%
+  filter(Term.Type %nin% c("Summer", "Winter")) %>%
+  select(Term, Random.Student.ID, Gender.Code, IPEDS.Ethnicity,
+         IPEDS.Ethnicity.URM.Non.URM, First.Generation.Flag, Academic.Level,
+         Academic.Program, Major.1.STEM.Flag, Major.1.College,
+         Major.2.STEM.Flag, Major.2.College, Entry.Enrollment.Type,
+         Academic.Standing.Status)
+program.clean$Random.Student.ID <- factor(program.clean$Random.Student.ID)
+
+# Student Profile Dataset
+profile <- read.csv("data/Student Profile Metric.csv")
+profile <- filter(profile, Cohort.Term.Year >= 2016) %>%
+  mutate(Random.Student.ID = factor(Random.Student.ID), .keep = "unused") %>%
+  select(Cohort.Term, Random.Student.ID, Degree.Term,
+         Full.Time.Part.Time.Code, Cohort.Student.Enrollment.Type, HS.GPA.Group,
+         HS.GPA, Transfer.GPA.Group, Transfer.GPA, Cohort.Time.to.Degree.Year,
+         Student.Orientation.Flag)
+
+# Student Programs and Profiles Joined together
+student_profiles <- left_join(program.clean, profile, by = "Random.Student.ID")
+student_profiles <- rename(student_profiles, Enrollment.Term = Cohort.Term)
+write.csv(student_profiles, "data/student_profiles_clean.csv")
+
+################################################################################
+# Grades Data
+################################################################################
+
+# Contains number of visits in the term, and flag for at least one visit
+clean_si_visit <- select(si_appt, Random.Student.ID, SLC.Attended.Flag,
+                         Term, Visit.Count..per.day.) %>%
+  group_by(Random.Student.ID, Term) %>%
+  summarise(attended.si = min(SLC.Attended.Flag),
+            count.visits = sum(Visit.Count..per.day.))
+
+# Import Grades Data, clean
+grades <- filter(grades, `Term Year` >= 2016)
+grades$`Random Course ID` <- factor(grades$`Random Course ID`)
+grades$`Term Type` <- factor(grades$`Term Type`)
+
+# Create Table for Grades of SI Classes
+si_grades <- filter(grades, `Term Type` %nin% c("Summer", "Winter"),
+                    `Random Course ID` %in% levels(si_appt$Random.Course.ID))
+si_grades$Random.Student.ID <- factor(si_grades$Random.Student.ID)
+
+# Contains Grades of all Students for All Courses With SI
+write.csv(si_grades, "data/si_grades.csv", row.names = FALSE)
+
+################################################################################
+# Filtered Student Profiles (Have taken at least one SI Course)
+################################################################################
+si_student_profiles <- filter(student_profiles,
+                              Random.Student.ID %in% levels(si_grades$Random.Student.ID))
