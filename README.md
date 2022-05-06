3f6e751e94e0090d76b38c05b1601c6cc39c0914
jshifman <josephshifman@gmail.com>
2022-05-05 21:25:45 -0700

    renamed files, edited data management to reduce redundancy and correct, and corrected models in pictures file

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# The impact of Supplemental Instruction on Equity Gaps at CSU, Chico
+# The impact of Supplemental Instruction on Equity Gaps at Chico State
 
 ## Introduction
 * Supplemental Instruction (**SI**) is designed to increase student success in historically difficult courses by utilizing peer-assisted study sessions. The SI leader has performed well in the course in a previous semester.
@@ -6,7 +6,14 @@
 * An **equity gap** refers to differences in educational outcomes and student success across race/ethnicity, socioeconomic status, gender, physical or mental abilities, and other demographic traits and intersectionalities.
 
 ## Workflow
-**Before running any files, you must first download the data files from the secure Box folder and put them into a folder called "data" in the main project repository on your local system. This data is private and must not be seen by eyes not working on the project. After downloading and moving data, run the entire file "data_management.R".
+Before running any files, you must first download the data files from the secure Box folder and put them into a folder called "data" in the main project repository on your local system. This data is private and must not be seen by eyes not working on the project. After downloading and moving data, run the entire file called "data_management.R". This will create many csv files (described below) and many data sets in your local environment. After it is done running, you can click the broomstick to clear your environment.
+
+_Data Sets Created:_
+
+- **grades.csv**
+  - Copy of the grades Excel file to be a more light-weight format
+- **course_level.csv**
+  - [CONTINUE]
 
 -----
 
--- data_management.R
+++ data_management.R
@@ -27,7 +27,8 @@ course.level <- grades %>% group_by(Random.Course.ID, Term.Year, Term.Type) %>%
 # Add number of SI visits to course level data
 si_visit <- read.csv('data/SLC Appointment.csv')
 temp3 <- si_visit %>% group_by(Random.Course.ID) %>%
-  summarise(SI.Visit.Num = sum(SLC.Attended.Flag))
+  summarise(SI.Visit.Num = sum(SLC.Attended.Flag), Total.visits = sum(Visit.Count..per.day.)) %>%
+  filter(Total.visits >= 10) # Removed classes are considered non-SI classes
 course.level <- course.level %>% left_join(temp3, by = 'Random.Course.ID')
 
 # Add covariates from course detail file
@@ -44,7 +45,7 @@ course.detail <- course.detail %>%
                                   696, 697, 699)) #independent study
 course.detail$course_num_clean <- droplevels(course.detail$course_num_clean)
 course.detail <- course.detail %>%
-  filter(Term.Year >= 2016) # Start of SI
+  filter(Term.Year >= 2016, Term.Year <= 2019) # Start of SI, before the Pandemic
 course.detail$Term.Type <- factor(course.detail$Term.Type)
 course.detail <- filter(course.detail, Term.Type %nin% c("Summer", "Winter"))
 course.detail <- droplevels(course.detail)
@@ -55,11 +56,9 @@ cols2 <- c("Random.Course.ID", "Course.Subject.and.Number", "Academic.Subject", 
            "GE.Class.Flag")
 course.detail %<>% mutate_at(cols2, factor)
 course.level$Random.Course.ID <- factor(course.level$Random.Course.ID)
-course.level <- filter(course.level, Random.Course.ID %in% levels(course.detail$Random.Course.ID))
-course.level <- droplevels(course.level)
 
 # Joining covariates
-course.level <- left_join(course.level, course.detail, by = 'Random.Course.ID')
+course.level <- inner_join(course.level, course.detail, by = 'Random.Course.ID')
 
 # Create flag for SI component
 course.level$SI.Visit.Num[is.na(course.level$SI.Visit.Num)] <- 0
@@ -92,17 +91,17 @@ program.clean <- program.clean[order(program.clean$Random.Student.ID),]
 program.clean <- program.clean[!duplicated(program.clean$Random.Student.ID),]
 
 # Student Profile Dataset
-# about 35% of students are not present in the profile dataset
-# therefore they have null values
 profile <- read.csv("data/Student Profile Metric.csv")
 profile <- filter(profile, Cohort.Term.Year >= 2016) %>%
   mutate(Random.Student.ID = factor(Random.Student.ID), .keep = "unused") %>%
   dplyr::select(Cohort.Term, Random.Student.ID, Degree.Term,
          Full.Time.Part.Time.Code, Cohort.Student.Enrollment.Type, HS.GPA.Group,
          HS.GPA, Transfer.GPA.Group, Transfer.GPA, Cohort.Time.to.Degree.Year,
-         Student.Orientation.Flag)
+         Student.Orientation.Flag, Cohort.Term.Year, One.Year.Retention)
 
 # Student Programs and Profiles Joined together
+# about 35% of students are not present in the profile dataset
+# therefore they have null values
 student_profiles <- left_join(program.clean, profile, by = "Random.Student.ID")
 student_profiles <- rename(student_profiles, Enrollment.Term = Cohort.Term)
 write.csv(student_profiles, "data/student_profiles_clean.csv", row.names = FALSE)
@@ -111,7 +110,7 @@ write.csv(student_profiles, "data/student_profiles_clean.csv", row.names = FALSE
 # Grades Data
 ################################################################################
 
-# Contains number of visits in the term, and flag for at least one visit
+# Contains number of visits in the term per student, and flag for at least one visit
 clean_si_visit <- dplyr::select(si_visit, Random.Course.ID, Random.Student.ID, SLC.Attended.Flag,
                          Term, Visit.Count..per.day.) %>%
   group_by(Random.Student.ID, Term) %>%
@@ -119,7 +118,7 @@ clean_si_visit <- dplyr::select(si_visit, Random.Course.ID, Random.Student.ID, S
             count.visits = sum(Visit.Count..per.day.))
 
 # Import Grades Data, clean
-grades <- filter(grades, Term.Year >= 2016)
+grades <- filter(grades, Term.Year >= 2016, Term.Year <= 2019)
 grades$Random.Course.ID <- factor(grades$Random.Course.ID)
 grades$Term.Type <- factor(grades$Term.Type)
 
@@ -139,22 +138,18 @@ si_student_profiles <- filter(student_profiles,
                               Random.Student.ID %in% levels(si_grades$Random.Student.ID))
 
 #############################################################################################
-#                         Create Grades data for only SI classes
+#                   Create Grades data for only SI classes
 #############################################################################################
-
-si_students <- read.csv("data/SLC Appointment.csv")
-profiles <- read.csv("data/Student Profile Metric.csv")
-courses <- read.csv("data/course_level.csv") %>% filter(SI.Component.Flag == 1)
-
-si_students <- si_students %>% dplyr::select(Term.Year, 
+si_students <- si_visit %>% dplyr::select(Term.Year, 
                                       Term.Type,
                                       Random.Course.ID,
                                       Random.Student.ID,
-                                      SLC.Attended.Flag)
+                                      SLC.Attended.Flag,
+                                      Visit.Count..per.day.)
 
 # Aggregate number of SI visits for each student in SLC dataset
 si_count <- si_students %>% group_by(Random.Course.ID, Random.Student.ID) %>%
-  dplyr::summarize(SI.Visit.Num = sum(SLC.Attended.Flag))
+  dplyr::summarize(SI.Visit.Num = sum(Visit.Count..per.day.))
 si_count$Random.Course.ID <- factor(si_count$Random.Course.ID)
 si_count$Random.Student.ID <- factor(si_count$Random.Student.ID)
 
@@ -165,8 +160,8 @@ si_grades <- dplyr::select(si_grades, Term.Year, Term.Type, Random.Course.ID, St
 
 # Create a flag for SI attended
 si_grades$SI.Attended <- ifelse(si_grades$SI.Visit.Num > 0, 1, 0)
-
 si_grades$SI.Attended[is.na(si_grades$SI.Attended)] <- 0
+si_grades$SI.Visit.Num[is.na(si_grades$SI.Visit.Num)] <- 0
 
 # Grades Data for only SI classes
 write.csv(si_grades, "data/grades_SI_classes.csv", row.names = FALSE)
@@ -174,9 +169,8 @@ write.csv(si_grades, "data/grades_SI_classes.csv", row.names = FALSE)
 #############################################################################################
 #                                 Data for matchit
 #############################################################################################
-
 grades <- read.csv("data/grades_SI_classes.csv")
-profiles <- read.csv("data/student_profiles_clean.csv") %>%
+profiles <- student_profiles %>%
   dplyr::select(c("Random.Student.ID",
            "IPEDS.Ethnicity",
            "IPEDS.Ethnicity.URM.Non.URM",
@@ -218,6 +212,9 @@ courses <- read.csv("data/Course Detail.csv") %>%
            "Honors.Class.Flag"))
 
 # Merge the data sets
+grades$Random.Student.ID<-factor(grades$Random.Student.ID)
+grades$Random.Course.ID <- factor(grades$Random.Course.ID)
+courses$Random.Course.ID <- factor(courses$Random.Course.ID)
 data <- grades %>% left_join(profiles) %>% left_join(courses) %>% drop_na(c(HS.GPA, 
                                                                             Student.Orientation.Flag,
                                                                             Major.1.STEM.Flag,
@@ -229,35 +226,6 @@ write.csv(data, "data/CEM_full_dataset.csv", row.names = FALSE)
 ################################################################################
 #                       Student analysis Dataset
 ################################################################################
-program <- read.csv("data/Student Program.csv")
-program.clean <- filter(program, Term.Year >= 2016) %>% # SI classes start in 2016
-  mutate(Term.Type = factor(Term.Type), .keep = "unused") %>%
-  filter(Term.Type %nin% c("Summer", "Winter")) %>%
-  dplyr::select(Term.Year, Random.Student.ID, Gender.Code, IPEDS.Ethnicity,
-                IPEDS.Ethnicity.URM.Non.URM, First.Generation.Flag, Academic.Level,
-                Academic.Program, Major.1.STEM.Flag, Major.1.College,
-                Major.2.STEM.Flag, Major.2.College, Entry.Enrollment.Type,
-                Academic.Standing.Status)
-
-program.clean$Random.Student.ID <- factor(program.clean$Random.Student.ID)
-# Remove duplicate rows
-program.clean <- program.clean[order(program.clean$Random.Student.ID),]
-program.clean <- program.clean[!duplicated(program.clean$Random.Student.ID),]
-
-# Student Profile Dataset
-profile <- read.csv("data/Student Profile Metric.csv")
-profile <- filter(profile, Cohort.Term.Year >= 2016) %>%
-  mutate(Random.Student.ID = factor(Random.Student.ID), .keep = "unused") %>%
-  filter(Cohort.Student.Enrollment.Type == "First-Time Freshman")%>%
-  dplyr::select(Cohort.Term, Cohort.Term.Year, Random.Student.ID, Degree.Term,
-                Full.Time.Part.Time.Code, Cohort.Student.Enrollment.Type, HS.GPA.Group,
-                HS.GPA, Transfer.GPA.Group, Transfer.GPA, Cohort.Time.to.Degree.Year,One.Year.Retention,
-                Student.Orientation.Flag)
-program.clean<-filter(program.clean, Random.Student.ID %in% levels(profile$Random.Student.ID))
-
-student_profiles <- left_join(program.clean, profile, by = "Random.Student.ID")
-student_profiles <- rename(student_profiles, Enrollment.Term = Cohort.Term)
-
 profile<- student_profiles %>% 
   dplyr::select(c("Random.Student.ID",
                   "IPEDS.Ethnicity",
@@ -304,6 +272,8 @@ courses <- read.csv("data/Course Detail.csv") %>%
                   "Honors.Class.Flag"))
 
 grades$Random.Student.ID<-factor(grades$Random.Student.ID)
+grades$Random.Course.ID <- factor(grades$Random.Course.ID)
+courses$Random.Course.ID <- factor(courses$Random.Course.ID)
 # Merge the data sets
 d <- grades %>% left_join(profile) %>% left_join(courses) %>%
   drop_na(c(HS.GPA, Student.Orientation.Flag, Major.1.STEM.Flag,
@@ -329,7 +299,7 @@ student_data <- dplyr::select(student_data, Term.Year, Random.Course.ID,
 course.level <- read.csv("data/course_level.csv")
 course.level$Random.Course.ID <- factor(course.level$Random.Course.ID)
 student_data$Random.Course.ID <- factor(student_data$Random.Course.ID)
-course.level <- filter(course.level, class.size >= 25, course_num_clean < 400) %>%
+course.level <- filter(course.level, class.size >= 20, course_num_clean < 300) %>% # More instances of lower division SI courses, and need substantial class size (20)
   dplyr::select(Random.Course.ID, class.size, class.average, dwf.rate, SI.Component.Flag)
 course.level <- inner_join(course.level, student_data, by = "Random.Course.ID")
 course.level <- drop_na(course.level, Term.Year)
@@ -341,6 +311,7 @@ course.level <- group_by(course.level, Random.Course.ID) %>%
             URM = sum(as.integer(URM)-1, na.rm = TRUE)/class.size,
             class.size, dwf.rate, class.average, SI.Component.Flag, Term.Year)
 course.level <- course.level[!duplicated(course.level$Random.Course.ID), ]
+course.level <- filter(course.level, Term.Year <= 2019)
 
 write.csv(course.level, "data/course_analysis_dataset.csv", row.names = FALSE)
 
