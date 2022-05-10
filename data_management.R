eb77e96cab2d009b67acb73ac21e798c42beb0f3
jshifman <josephshifman@gmail.com>
2022-05-10 10:24:06 -0700

    removed redundant dataset

--- data_management.R
+++ data_management.R
@@ -143,35 +143,6 @@ si_student_profiles <- filter(student_profiles,
                               Random.Student.ID %in% levels(si_grades$Random.Student.ID))
 
 #############################################################################################
-#   Create Grades data for only SI classes [Redundant, same as above]
-#############################################################################################
-si_students <- si_visit %>% dplyr::select(Term.Year, 
-                                      Term.Type,
-                                      Random.Course.ID,
-                                      Random.Student.ID,
-                                      SLC.Attended.Flag,
-                                      Visit.Count..per.day.)
-
-# Aggregate number of SI visits for each student in SLC dataset
-si_count <- si_students %>% group_by(Random.Course.ID, Random.Student.ID) %>%
-  dplyr::summarize(SI.Visit.Num = sum(Visit.Count..per.day.))
-si_count$Random.Course.ID <- factor(si_count$Random.Course.ID)
-si_count$Random.Student.ID <- factor(si_count$Random.Student.ID)
-
-si_grades <- si_grades %>% left_join(si_count)
-
-si_grades <- dplyr::select(si_grades, Term.Year, Term.Type, Random.Course.ID, Student.Class.Official.Grade,
-                 Random.Student.ID, SI.Visit.Num, Student.Class.Unit.Passed, Student.Class.Unit.Attempted)
-
-# Create a flag for SI attended
-si_grades$SI.Attended <- ifelse(si_grades$SI.Visit.Num > 0, 1, 0)
-si_grades$SI.Attended[is.na(si_grades$SI.Attended)] <- 0
-si_grades$SI.Visit.Num[is.na(si_grades$SI.Visit.Num)] <- 0
-
-# Grades Data for only SI classes
-write.csv(si_grades, "data/grades_SI_classes.csv", row.names = FALSE)
-
-#############################################################################################
 #                                 Data for matchit
 #############################################################################################
 grades <- read.csv("data/grades_SI_classes.csv")
